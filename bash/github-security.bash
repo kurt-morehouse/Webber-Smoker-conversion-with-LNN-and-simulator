2223619463d4291f01e25d7f89c83d6877cc9e0b
Kurt Morehouse <kurt.morehouse@gmail.com>
2026-08-31 10:31:11 -0400

    add engineering workbenchh

---
+++ bash/github-security.bash
@@ -0,0 +1,25 @@
+# set up a clean GitHub workflow.
+
+git --version
+# Then set your name and email.
+git config --global user.name 'kurt morehouse' user.email 'kurt.morehouse@gmail.com'
+
+#cThen we need an SSH key.
+ssh-keygen -ed25519v2 -c kurt.morehouse@gmail.com
+
+val $'ssh-agent -s'
+ssh -add ~/.ssh/ed25519v2
+cat ~/.ssh/ed25519v2.pub
+ # paste it into GitHub under SSH keys.
+ # Then test
+ ssh -T git@github.com.
+
+# with a repo for this project?
+# from your project folder,
+git init
+git remote add origin git@github.com:kurt-morehouse/repo.git
+git pull origin main
+# then commit and push.
+# If origin already exists, we can just use git remote set-url instead.
+# Want to paste the output of git remote dash v, so I can guide you precisely?
+
